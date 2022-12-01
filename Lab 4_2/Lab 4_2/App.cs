9232a0d16e2814ee3483768a089742bae62b73c9
User <mrdoggydog37@gmail.com>
2022-12-01 21:07:41 +0300

    Added project 5

---
+++ Lab 4_2/Lab 4_2/App.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace Lab_4_2
+{
+    class RSA
+    {
+        static void Main()
+        {
+            try
+            {
+                UnicodeEncoding ByteConverter = new UnicodeEncoding();
+
+                Console.Write("Введите сообщение: ");
+
+                byte[] dataToEncrypt = ByteConverter.GetBytes(Console.ReadLine());
+                byte[] encryptedData;
+                byte[] decryptedData;
+
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+
+                    decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+
+                    Console.WriteLine("Расшифрованное сообщение: {0}", ByteConverter.GetString(decryptedData));
+                }
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Не удалось расшифровать сообщение.");
+            }
+        }
+
+        public static byte[] RSAEncrypt(byte[] DataToEncrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] encryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    encryptedData = RSA.Encrypt(DataToEncrypt, DoOAEPPadding);
+                }
+                return encryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.Message);
+
+                return null;
+            }
+        }
+
+        public static byte[] RSADecrypt(byte[] DataToDecrypt, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        {
+            try
+            {
+                byte[] decryptedData;
+                using (RSACryptoServiceProvider RSA = new RSACryptoServiceProvider())
+                {
+                    RSA.ImportParameters(RSAKeyInfo);
+
+                    decryptedData = RSA.Decrypt(DataToDecrypt, DoOAEPPadding);
+                }
+                return decryptedData;
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine(e.ToString());
+
+                return null;
+            }
+        }
+    }
+}
---
+++ Lab 5/Lab 5/App.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class RSASign
+{
+    static void Main()
+    {
+        try
+        {
+            UTF8Encoding ByteConverter = new UTF8Encoding();
+
+            Console.Write("Введите сообщение: ");
+            string dataString = Console.ReadLine();
+
+            byte[] originalData = ByteConverter.GetBytes(dataString);
+            byte[] signedData;
+
+            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+
+            RSAParameters Key = RSAalg.ExportParameters(true);
+
+            signedData = HashAndSignBytes(originalData, Key);
+
+            if (VerifySignedHash(originalData, signedData, Key))
+            {
+                Console.WriteLine("ЭЦП принадлежит этому сообщению");
+            }
+            else
+            {
+                Console.WriteLine("ЭЦП не принадлежит этому сообщению");
+            }
+        }
+        catch (ArgumentNullException)
+        {
+            Console.WriteLine("ЭЦП не проверено");
+        }
+    }
+    public static byte[] HashAndSignBytes(byte[] DataToSign, RSAParameters Key)
+    {
+        try
+        {
+            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+
+            RSAalg.ImportParameters(Key);
+
+            return RSAalg.SignData(DataToSign, SHA256.Create());
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.Message);
+
+            return null;
+        }
+    }
+
+    public static bool VerifySignedHash(byte[] DataToVerify, byte[] SignedData, RSAParameters Key)
+    {
+        try
+        {
+            RSACryptoServiceProvider RSAalg = new RSACryptoServiceProvider();
+
+            RSAalg.ImportParameters(Key);
+
+            return RSAalg.VerifyData(DataToVerify, SHA256.Create(), SignedData);
+        }
+        catch (CryptographicException e)
+        {
+            Console.WriteLine(e.Message);
+
+            return false;
+        }
+    }
+}
